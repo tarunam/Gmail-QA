24fa1718266a5388c4cf394f588b64e003f7d27b
tarunam <tarunam@gmail.com>
2016-01-22 13:27:45 +0530

    Add Constants.java

---
+++ Constants.java
@@ -0,0 +1,30 @@
+/**
+ * This class is used for storing the constants used in the script
+ */
+package GmailPackage;
+
+/**
+ * @author tarunam.nair
+ *
+ */
+
+public class Constants {
+	
+		//The url for the gmail
+		public static final String URL = "https://www.gmail.com/";
+		
+		//The xlsx file used for parameterising the login details
+		public static final String Filename = "GmailLogin.xlsx";
+	    public static final String Sheetname = "Login";
+	    
+	    //The email used to send the email
+	    public static final String Toemail = "hiring@coriolis.co.in";
+	    public static final String Subjemail = "Assignment for QA Position";
+	    
+	    public static final String Mailbody = "Name :- Tarunam"+"\n"+"Email :- tarunam@gmail.com"+ "\n" +"Phone :- 9665605154"+"\n"+"Link to github :- ";    
+	
+	    
+	}
+
+
+
